62af5e15ca56061cda6073be546f2bf13a515a31
David Rubert <david.rubert@gmail.com>
2012-03-11 21:14:51 +0000

    Ultimos cambios

--- js/main.js
+++ js/main.js
@@ -7,8 +7,8 @@ require.config({
     underscore: 'libs/underscore/underscore-min', // https://github.com/amdjs
     backbone: 'libs/backbone/backbone-min', // https://github.com/amdjs
     sinon: 'libs/sinon/sinon.js',
-    async: 'libs/requirejs-plugins/src/async',
-    depend: 'libs/requirejs-plugins/src/depend',
+    async: 'libs/require/async',
+    depend: 'libs/require/depend',
 
     // Require.js plugins
     text: 'libs/require/text',
